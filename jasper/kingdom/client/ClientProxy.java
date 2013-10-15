4d9502a9484322d933a87886bae0172a58996872
oplek <jasper@oplek.net>
2013-10-15 17:50:58 -0400

    Create ClientProxy.java

---
+++ jasper/kingdom/client/ClientProxy.java
@@ -0,0 +1,11 @@
+package jasper.kingdom.client;
+
+import jasper.kingdom.CommonProxy;
+import net.minecraftforge.client.MinecraftForgeClient;
+
+public class ClientProxy extends CommonProxy {
+	
+	public void registerRenderers() {
+		
+	}
+}
